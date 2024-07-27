f6f97f7a7feaf0072cb99f4896754f613f23171c
Deady007 <parmarviral397@gmail.com>
2024-07-27 14:09:58 +0530

    COMMIT FR

--- fun/PROPOSAL/untoldcoding.js
+++ fun/PROPOSAL/untoldcoding.js
@@ -1,7 +1,7 @@
 var i = 0;
 var txt1 =
-  "Bakudi, from the first time we met, there was something magical between us. Every moment we spend together, whether it's laughing, talking, or just being in each other's presence, feels incredibly special to me. << You’ve become such an important part of my life, and I can’t help but smile whenever I think about you. Your kindness, your smile, and the way you care for me are things I treasure more than words can express. << Today, I want to take a step forward and make our bond even stronger. I’ve realized that my feelings for you go beyond just friendship. I care about you deeply, and I want to be more than just friends. << So, with all my heart, I’m asking you something very important. [Take a deep breath and hold her hands.] << BAKudi, would you do me the honor of being my girlfriend? I promise to cherish you, support you, and make every moment we share together as wonderful as it can be. I want to continue building beautiful memories with you, and I hope you feel the same way. << Will you be my girlfriend?";
-var speed = 35;
+  " My Dearest Bakudi, I love you.<< I love your eyes, your beauty, your delicate smile, your cuteness. 💖<< I love your pain, your tears, your face, your sweet words. I love your forgiving nature, your shyness, your love, your desire, your ability to make every day special for me. <<I love your true self, and I love the person you consider your own. You mean the world to me, and I cherish every moment with you.<< Will you make my dreams come true and be the Radha to my Krishna, the Aditi to my Bunny, and my Bakudi forever?";
+var speed = 5;
 
 typeWriter();
 
